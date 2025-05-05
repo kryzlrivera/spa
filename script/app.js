cd07325e6486aa17b30022608d3bc1f25ab1b15a
Donnel_prog <donnel.simbajon@urios.edu.ph>
2025-05-05 04:07:19 +0000

    first commit

--- script/app.js
+++ script/app.js
@@ -1,4 +1,4 @@
 Alpine.store("settings", {
-    apiBaseUrl:"http://172.17.100.14:3332/martinito3/api/",  
-    appName: "Electronic store",  
+    apiBaseUrl:"http://172.17.100.14:3332/default/api/",  
+    appName: "Ecoomerce store",  
 });
